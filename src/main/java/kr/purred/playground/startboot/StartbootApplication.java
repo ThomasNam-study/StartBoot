60d2a580dd48f74c95adb48ca21f0f20913b1448
RED <purred@udid.co.kr>
2017-12-07 10:28:49 +0900

    테스트 시작

---
+++ src/main/java/kr/purred/playground/startboot/StartbootApplication.java
@@ -0,0 +1,12 @@
+package kr.purred.playground.startboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartbootApplication.class, args);
+	}
+}
