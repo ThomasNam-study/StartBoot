0739f95fdecda405828348f16b28e3623b4fff21
RED <purred@naver.com>
2018-01-28 17:20:18 +0900

    12장 까지 완료

--- src/main/java/kr/purred/playground/startboot/StartbootApplication.java
+++ src/main/java/kr/purred/playground/startboot/StartbootApplication.java
@@ -2,13 +2,21 @@ package kr.purred.playground.startboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StartbootApplication
+public class StartbootApplication extends SpringBootServletInitializer
 {
 
 	public static void main (String[] args)
 	{
 		SpringApplication.run (StartbootApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder builder)
+	{
+		return builder.sources (SpringBootApplication.class);
 	}
 }
